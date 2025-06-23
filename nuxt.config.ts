b77838dc760cb48f1cc7c4d768ca00c089c5acaf
João Pedro <joaoxstone@gmail.com>
2025-06-23 10:59:17 -0300

    styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-   modules: ['@pinia/nuxt','@nuxt/image'],
-   image: {
+  modules: ['@pinia/nuxt', '@nuxt/image'],
+  image: {
     quality: 80,
   },
+  css: [
+    '~/public/css/style.css',
+  ],
   server: {
     port: 3000, // muda a porta padrão de 3000 para 3001
     host: '0.0.0.0' // opcional: permite acesso externo
