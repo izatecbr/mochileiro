44c970bb24db29b29baaa83d5f928ca6a354d599
João Pedro <joaoxstone@gmail.com>
2025-06-26 14:47:22 -0300

    icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxt/image'],
+  modules: ['@pinia/nuxt', '@nuxt/image', '@nuxt/icon'],
   runtimeConfig: {
     public: {
       pexelsKey: ''
