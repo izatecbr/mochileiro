7fb6ff19d731deeececfaa1a503216c6eb8ef7c9
João Pedro <joaoxstone@gmail.com>
2025-06-26 12:59:37 -0300

    overview

---
+++ components/experiencias/BottomOverview.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card-bottom">
+    <span> {{ valor?.moeda }} {{ valor?.formaPagamento }}</span>
+    <div class="d-md-flex">
+      <NuxtLink class="anfitriao" :to="`/anfitriao/${anfitriao?.id}`">{{ anfitriao?.legenda }}</NuxtLink>
+      <NuxtLink>{{
+        anfitriao }}</NuxtLink>
+      <NuxtLink>
+        {{ anfitriao?.site }}
+      </NuxtLink>
+      <NuxtLink>
+        {{ anfitriao?.instagram }}
+      </NuxtLink>
+      <NuxtLink v-if="anfitriao?.telefone?.whatsapp">
+        {{ anfitriao?.telefone?.numero }}
+      </NuxtLink>
+      <NuxtLink>
+        {{ anfitriao?.email }}
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps(['valor', 'anfitriao'])
+</script>
+
+<style scoped lang="css">
+.card-bottom {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 0.4rem;
+  border-radius: 12px;
+  border: 1px solid var(--border, #E7E6E6);
+  box-shadow: 0px 10px 40px 0px rgba(0, 0, 0, 0.05);
+  background-color: white;
+}
+
+.anfitriao {
+  text-decoration: underline;
+}
+</style>
--- components/experiencias/Overview.vue
+++ components/experiencias/Overview.vue
@@ -1,24 +1,21 @@
 <template>
   <h2 class="text-30">Introdução</h2>
   <p class="mt-20">
-    {{overview||'-'}}
+    {{ overview || '-' }}
   </p>
 
   <h3 class="text-20 fw-500 mt-20">Planejamento</h3>
-  <ul v-for="aventura in aventuras" :key="aventura.id" class="ulList mt-20">
-    <h4 class="text-18 fw-500">{{aventura.legenda}}</h4>
-    <li v-for="atividade in aventura.atividadesList"  >
-      <i v-if="atividade.escolhida"
-          class="icon-check flex-center text-10 size-24 rounded-full text-green-2 bg-green-1 mr-15"
-      ></i>
-      <i v-else
-          class="icon-cross flex-center text-10 size-24 rounded-full text-red-3 bg-red-4 mr-15"
-      ></i>
-      <NuxtLink style=" text-decoration: underline;" class=""  :to="`/atividades/${atividade.id}`">
-       {{ atividade.legenda }}  {{ atividade?.duracao ? `- (${atividade.duracao})` : '' }}
+  <ul v-for="aventura in aventuras" :key="aventura.id" class="mt-20">
+    <h4 class="text-18 fw-500">{{ aventura.legenda }}</h4>
+    <li v-for="atividade in aventura.atividadesList">
+      <NuxtLink  class="d-flex mt-10 justify-start items-center" :to="`/atividades/${atividade.id}`">
+        <i v-if="atividade.escolhida"
+          class="icon-check flex-center text-10 size-24 rounded-full text-green-2 bg-green-1 mr-15"></i>
+        <i v-else class="icon-cross flex-center text-10 size-24 rounded-full text-red-3 bg-red-4 mr-15"></i>
+        <p class="label-overview" >{{ atividade.legenda }} {{ atividade?.duracao ? `- (${atividade.duracao})` : '' }}</p>
       </NuxtLink>
       <br>
-      <span class="text-13" >{{ atividade?.valor }}</span>
+      <ExperienciasBottomOverview :valor="atividade?.valor" :anfitriao="atividade?.anfitriaoObject"/>
     </li>
   </ul>
 </template>
@@ -26,6 +23,12 @@
 <script setup>
 const props = defineProps(['overview', 'aventuras'])
 
-console.log('Overview props:', props.aventuras)
+console.log('Overview prop:', props.aventuras)
 
 </script>
+
+<style scoped lang="css">
+.label-overview{
+  text-decoration: underline;
+}
+</style>
