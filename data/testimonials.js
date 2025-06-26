4a16927642dba51997a1bf32cdacd9479d001b83
Gleyson Oliveira <izapay4devs@gmail.com>
2025-06-26 09:56:25 -0300

    conteudos

--- data/testimonials.js
+++ data/testimonials.js
@@ -48,19 +48,19 @@ export const testimonialsOne = [
 
 export const testimonialsTwo = [
   {
-    name: "Albert Cole",
+    name: "Kayla Deodato",
     role: "Designer",
     image: "/img/avatars/1/1.jpg",
     comment: `O blog traz informações importantes sobre o pré-planejamento da viagem, detalhando preços e indicando os estabelecimentos que oferecem os serviços e suporte necessários. Isso ajuda muito a organizar tudo com mais segurança e clareza.`,
   },
   {
-    name: "Alison Dawn",
+    name: "Eberte Sampaio",
     role: "WordPress Developer",
     image: "/img/avatars/1/2.jpg",
     comment: `O site explica bem cada etapa das aventuras, facilitando o planejamento e evitando surpresas. Os detalhes sobre preparo e desafios tornam o conteúdo confiável e útil para quem quer viver experiências parecidas.`,
   },
   {
-    name: "Daniel Parker",
+    name: "Evandro Lima",
     role: "Front-end Developer",
     image: "/img/avatars/1/3.jpg",
     comment: `Adoro acompanhar esse blog! As histórias são super inspiradoras e me ajudam muito a planejar minhas viagens. Sempre encontro dicas valiosas e relatos reais que parecem um convite para a aventura. A comunidade também é muito ativa, adoro ler os comentários e trocar experiências. Recomendo para todo mundo que ama viajar!`,
