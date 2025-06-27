941cdcc135377f72bfea34871c63afb5a6625b69
Gleyson Oliveira <izapay4devs@gmail.com>
2025-06-27 15:36:40 -0300

    Update data.vue

--- pages/data.vue
+++ pages/data.vue
@@ -1,8 +1,10 @@
 <!-- src/views/Home.vue -->
 <template>
+  <section class="hero -type-3">
   <div class="grid-container">
     <DBView v-for="n in 6" :key="n" />
   </div>
+    </section>/
 </template>
 
 <script setup>
