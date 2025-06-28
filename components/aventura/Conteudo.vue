b7c5358bf7cef8ba74567581bebbf178dee3a9fd
Gleyson Oliveira <izapay4devs@gmail.com>
2025-06-28 18:57:50 -0300

    aventuras

--- components/aventura/Conteudo.vue
+++ components/aventura/Conteudo.vue
@@ -21,8 +21,8 @@
 
         </div>
         <AventuraValorAnfitriacao style="margin-top: 4px;" :valor="objeto?.valor" :anfitriao="objeto?.anfitriaoObject"/>
-        <div class="line mt-25 mb-25"></div>
         <AventuraCotacao  :atividades="objeto?.atividadesList" />
+
       </div>
     </div>
   </section>
